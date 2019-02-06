694c2bfa8c05b52607d9ed79a931f3bdf805e480
obedm503 <obedm503@gmail.com>
2019-02-05 19:45:05 -0600

    add Seller#confirmedAt and email must end in edu

--- src/server/seller/seller.entity.ts
+++ src/server/seller/seller.entity.ts
@@ -1,6 +1,29 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  registerDecorator,
+  ValidationOptions,
+  IsDate,
+} from 'class-validator';
 import { BaseEntity, Column, Entity, Unique } from 'typeorm';
 import { Created, PrimaryKey, Updated } from '../util/db';
+
+function IsEdu(validationOptions?: ValidationOptions) {
+  return function(object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isEdu',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: any) {
+          return typeof value === 'string' && value.endsWith('.edu');
+        },
+      },
+    });
+  };
+}
 
 @Entity()
 @Unique(['email'])
@@ -14,19 +37,24 @@ export class Seller extends BaseEntity {
   @Updated()
   readonly updatedAt: Date;
 
-  @Column({ nullable: true })
-  @IsString()
-  name?: string;
-
   @Column()
-  @IsEmail()
   @IsNotEmpty()
+  @IsEmail()
+  @IsEdu({ message: 'email must be valid school email' })
   email: string;
 
   @Column()
-  @IsString()
   @IsNotEmpty()
+  @IsString()
   passwordHash: string;
+
+  @Column({ nullable: true })
+  @IsDate()
+  confirmedAt?: Date;
+
+  @Column({ nullable: true })
+  @IsString()
+  name?: string;
 
   @Column({ nullable: true })
   @IsString()
