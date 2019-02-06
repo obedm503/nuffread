a596c428c9475726904bb686b1ec29a68fd7c1e1
obedm503 <obedm503@gmail.com>
2019-02-06 12:33:32 -0600

    different routes depending on the user

--- src/shared/app.tsx
+++ src/shared/app.tsx
@@ -3,10 +3,13 @@ import {
   IntrospectionFragmentMatcher,
 } from 'apollo-cache-inmemory';
 import { Kind } from 'graphql';
+import gql from 'graphql-tag';
 import * as React from 'react';
+import { Query } from 'react-apollo';
 import { RouteProps, withRouter } from 'react-router';
 import { Buy } from './buy';
-import { Footer, Routes } from './components';
+import { Error, Footer, Routes } from './components';
+import { Join } from './join';
 import { Launch } from './launch';
 import { Sell } from './sell';
 import { IsDesktopProvider } from './state/desktop';
@@ -29,21 +32,66 @@ export const createCache = () =>
     }),
   });
 
-const routes: RouteProps[] =
-  process.env.MODE !== 'ready'
-    ? [{ component: Launch }]
-    : [
-        { path: '/sell', component: Sell },
-        { path: '/', component: Buy },
-        { component: () => <p>Page not Found</p> },
-      ];
+const makeRoutes = (user?: GQL.User): RouteProps[] => {
+  if (process.env.MODE !== 'ready') {
+    return [{ component: Launch }];
+  }
 
-export const App = withRouter(({ match }) => (
-  <IsDesktopProvider>
-    <UserProvider value={{ user: {} } as any}>
-      <Routes routes={routes} key={match.url} />
+  let routes: RouteProps[] = [
+    { path: '/login', exact: true, component: () => <div>Login page</div> },
+    { path: '/join', exact: true, component: Join },
+  ];
 
-      <Footer />
-    </UserProvider>
-  </IsDesktopProvider>
-));
+  if (!user) {
+    routes.push({ path: '/', component: Buy });
+  } else if (user.__typename === 'Seller') {
+    routes.push({ path: '/', component: Sell });
+  } else if (user.__typename === 'Admin') {
+    routes.push({ path: '/', component: () => <div>Admin page</div> });
+  }
+  return routes;
+};
+
+const ME = gql`
+  query GetMe {
+    me {
+      ... on Seller {
+        id
+        email
+        name
+        photo
+      }
+      ... on Admin {
+        id
+        email
+      }
+    }
+  }
+`;
+
+export const App = withRouter(({ match }) => {
+  return (
+    <Query<GQL.IQuery> query={ME}>
+      {({ loading, data, error }) => {
+        if (loading || !data) {
+          return null;
+        }
+        if (error) {
+          return <Error value={error} />;
+        }
+
+        const me = data.me || undefined;
+        const routes = makeRoutes(me);
+        return (
+          <IsDesktopProvider>
+            <UserProvider value={{ user: me }}>
+              <Routes routes={routes} key={match.url} />
+
+              <Footer />
+            </UserProvider>
+          </IsDesktopProvider>
+        );
+      }}
+    </Query>
+  );
+});
