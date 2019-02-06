e0888d17a1cca306c4a07c8519ca10a143e33b0c
obedm503 <obedm503@gmail.com>
2019-02-05 20:23:14 -0600

    self contained TopNav

--- src/shared/components/top-nav.tsx
+++ src/shared/components/top-nav.tsx
@@ -1,28 +1,52 @@
+import { Container, Navbar } from 'bloomer';
 import * as React from 'react';
-import { Navbar, Container } from 'bloomer';
 import { classes, Color } from '../util';
+
+type RenderChildren =
+  | React.ReactNode
+  | ((props: {
+      onClick: React.MouseEventHandler;
+      isActive: boolean;
+    }) => React.ReactNode);
 
 type Props = {
   className?: string;
   isTopColor?: Color;
   isColor?: Color;
   hasShaddow?: boolean;
+  children: RenderChildren;
 };
-export const TopNav: React.SFC<Props> = ({
-  children,
-  className,
-  isTopColor: isTopColor,
-  isColor,
-  hasShaddow = true,
-}) => (
-  <Navbar
-    className={classes('is-fixed-top', className, {
-      [`is-top-${isTopColor}`]: isTopColor,
-      [`is-${isColor}`]: isColor,
-    })}
-  >
-    {hasShaddow === false ? null : <div className="shadow" />}
+export class TopNav extends React.PureComponent<Props, { isActive: boolean }> {
+  state = { isActive: false };
+  onClick: React.MouseEventHandler = () =>
+    this.setState(({ isActive }) => ({ isActive: !isActive }));
 
-    <Container>{children}</Container>
-  </Navbar>
-);
+  render() {
+    const {
+      children,
+      className,
+      isTopColor,
+      isColor,
+      hasShaddow = true,
+    } = this.props;
+    return (
+      <Navbar
+        className={classes('is-fixed-top', className, {
+          [`is-top-${isTopColor}`]: isTopColor,
+          [`is-${isColor}`]: isColor,
+        })}
+      >
+        {hasShaddow === false ? null : <div className="shadow" />}
+
+        <Container>
+          {typeof children === 'function'
+            ? (children as Function)({
+                isActive: this.state.isActive,
+                onClick: this.onClick,
+              })
+            : children}
+        </Container>
+      </Navbar>
+    );
+  }
+}
