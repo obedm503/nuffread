bf5ecab808a72e88a799ff6bad2293417b7d4cb3
obedm503 <obedm503@gmail.com>
2019-02-12 14:27:15 -0600

    expose seller listings

--- src/server/seller/seller.resolver.ts
+++ src/server/seller/seller.resolver.ts
@@ -1,8 +1,24 @@
+import { Listing } from '../listing/listing.entity';
+import { AuthenticationError, AuthorizationError } from '../util';
 import { IResolver } from '../util/types';
 import { Seller } from './seller.entity';
 
 export const SellerResolver: IResolver<GQL.ISeller, Seller> = {
   name(seller) {
     return seller.name || seller.email;
+  },
+  async listings(seller, args, { user }) {
+    if (!user) {
+      throw new AuthenticationError();
+    }
+    if (!(user instanceof Seller)) {
+      throw new AuthorizationError();
+    }
+
+    const listings = await Listing.find({
+      where: { sellerId: seller.id },
+      order: { createdAt: 'DESC' },
+    });
+    return (listings as any) as GQL.IListing[];
   },
 };
