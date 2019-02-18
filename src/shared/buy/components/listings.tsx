a2e7271ad696acddf7aa0d79d76debdfb1e64060
obedm503 <obedm503@gmail.com>
2019-02-17 18:22:51 -0600

    Details is now a prop instead of a fixed component

--- src/shared/buy/components/listings.tsx
+++ src/shared/buy/components/listings.tsx
@@ -1,56 +1,24 @@
 import { Column, Columns, Container } from 'bloomer';
 import * as React from 'react';
-import { Query } from 'react-apollo';
-import { Error } from '../../components';
-import { GET_LISTING } from '../../queries';
 import { Listing } from './listing';
-import { ListingDetails } from './listing-details';
-import { SellerDetails } from './seller-details';
-
-const Details: React.SFC<{
-  id: string;
-  base: string;
-}> = ({ id, base }) => (
-  <Query<GQL.IQuery> query={GET_LISTING} variables={{ id }}>
-    {({ loading, error, data }) => {
-      if (loading) {
-        return null;
-      }
-
-      if (error || !data) {
-        return <Error value={error} />;
-      }
-
-      const listing = data.listing;
-      if (!listing) {
-        return null;
-      }
-
-      return (
-        <>
-          <ListingDetails listing={listing} base={base} />
-          <SellerDetails listingId={listing.id} />
-        </>
-      );
-    }}
-  </Query>
-);
 
 export class ListingsMain extends React.PureComponent<{
-  listingId?: string;
+  id?: string;
   isDesktop: boolean;
   onClick: any;
-  listings: GQL.IListing[];
+  listings: Array<GQL.IListing | GQL.IBook>;
   base: string;
+  children: (props: { id: string; base: string }) => React.ReactNode;
 }> {
   onClick = id => () => this.props.onClick(id);
   render() {
-    const { listingId, isDesktop, listings, base } = this.props;
+    const { id, isDesktop, listings, base, children } = this.props;
+    const Details = children as Function;
     return (
-      <Container className={listingId ? 'buy-main' : ''}>
+      <Container className={id ? 'buy-main' : ''}>
         <Columns>
-          {isDesktop || !listingId ? (
-            <Column className={listingId ? 'scrolls' : ''}>
+          {isDesktop || !id ? (
+            <Column className={id ? 'scrolls' : ''}>
               {listings.map((listing, i) => {
                 if (!listing) {
                   return null;
@@ -59,7 +27,7 @@ export class ListingsMain extends React.PureComponent<{
                   <Listing
                     key={listing.id}
                     isFirst={i === 0}
-                    isActive={listingId === listing.id}
+                    isActive={id === listing.id}
                     onClick={this.onClick(listing.id)}
                     listing={listing}
                   />
@@ -68,9 +36,9 @@ export class ListingsMain extends React.PureComponent<{
             </Column>
           ) : null}
 
-          {listingId ? (
-            <Column>
-              <Details id={listingId} base={base} />
+          {id ? (
+            <Column style={{ width: '50%' }}>
+              <Details id={id} base={base} />
             </Column>
           ) : null}
         </Columns>
