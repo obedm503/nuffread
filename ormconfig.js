a2070bdd70984c9cfe01866e05c44529ea609950
obedm503 <obedm503@gmail.com>
2019-03-24 22:23:45 -0500

    install typeorm cli

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,8 @@
+// @ts-check
 const { resolve } = require('path');
 require('dotenv-safe').config({
-  path: resolve('./api/.env'),
-  sample: resolve('./api/.env.example'),
+  path: resolve('api/.env'),
+  sample: resolve('api/.env.example'),
 });
 
 module.exports = {
@@ -9,7 +10,7 @@ module.exports = {
   migrations: 'api/src/migrations/*.ts',
   url: process.env.DATABASE_URL,
   cli: {
-    subscribersDir: './api/src/subscribers',
-    migrationsDir: './api/src/migrations',
+    subscribersDir: 'api/src/subscribers',
+    migrationsDir: 'api/src/migrations',
   },
 };
