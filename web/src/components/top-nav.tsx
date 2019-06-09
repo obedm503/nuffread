435c1b23c81f0ae2d46dd0b89847fab50e0a7138
obedm503 <obedm503@gmail.com>
2019-06-08 20:11:27 -0700

    set page title same as navbar title

--- web/src/components/top-nav.tsx
+++ web/src/components/top-nav.tsx
@@ -1,16 +1,22 @@
 import { IonHeader, IonTitle, IonToolbar } from '@ionic/react';
 import * as React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 type Props = {
   toolbar?: React.ReactNode;
+  title?: string;
 };
 export class TopNav extends React.PureComponent<Props> {
   render() {
     const { children, toolbar = null } = this.props;
+    const title = this.props.title || 'nuffread';
     return (
       <IonHeader>
         <IonToolbar color="primary">
-          <IonTitle>nuffread</IonTitle>
+          <IonTitle>{title}</IonTitle>
+          <Helmet>
+            <title>{title}</title>
+          </Helmet>
 
           {children}
         </IonToolbar>
