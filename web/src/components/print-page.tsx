86edbf85779057e49a11a8fae126f3ba5bc37244
obedm503 <obedm503@gmail.com>
2019-06-11 21:36:31 -0700

    cleanup bulma-based components

--- web/src/components/print-page.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Section } from './section';
-
-export const PrintPage: React.SFC = ({ children }) => (
-  <Section className="print-page">
-    <div className="content">{children}</div>
-  </Section>
-);
