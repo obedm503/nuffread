a580963986b44a4c03bc90ebf351e78646a70b51
obedm503 <obedm503@gmail.com>
2019-06-17 22:25:25 -0700

    same view for /listings for mobile and desktop

--- web/src/buy/components/listings.tsx
+++ web/src/buy/components/listings.tsx
@@ -1,12 +1,16 @@
-import { IonContent, IonItem, IonLabel, IonList } from '@ionic/react';
-import { Omit } from 'lodash';
+import {
+  IonButtons,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+} from '@ionic/react';
 import * as React from 'react';
-import { RouteComponentProps, RouteProps } from 'react-router';
-import { Footer, Routes } from '../../components';
+import { Redirect, RouteComponentProps, RouteProps } from 'react-router';
+import { Footer, IonBackButton, IonRoutes, TopNav } from '../../components';
 import { Listing } from '../../components/listing';
 import { SearchBar } from '../../components/search-bar';
 import { IBook, IListing } from '../../schema.gql';
-import { IsDesktop } from '../../state/desktop';
 import { Nav } from './nav';
 
 type IListings = Array<IListing | IBook>;
@@ -20,136 +24,87 @@ type Props = {
   searchValue?: string;
 };
 
-class List extends React.PureComponent<
-  Omit<Props, 'component' | 'onSearch' | 'searchValue'>
-> {
-  onClick = id => () => this.props.onClick(id);
-  render() {
-    if (!this.props.listings.length) {
-      return (
-        <IonList lines="none">
-          <IonItem color="white">
-            <IonLabel>Found nothing...</IonLabel>
-          </IonItem>
-        </IonList>
-      );
-    }
-    return (
-      <IonList lines="none">
-        {this.props.listings.map((listing, i) => {
-          if (!listing) {
-            return null;
-          }
-          return (
-            <Listing
-              key={listing.id}
-              priceColor={i === 0 ? 'success' : undefined}
-              onClick={this.onClick(listing.id)}
-              listing={listing}
-            />
-          );
-        })}
-      </IonList>
-    );
-  }
-}
-
-const Desktop: React.SFC<Props> = ({
-  component,
-  id,
-  base,
-  onSearch,
-  searchValue = '',
-  ...props
-}) => {
-  return (
-    <>
-      <Nav>
-        <SearchBar onSearch={onSearch} searchValue={searchValue} />
-      </Nav>
-
-      <IonContent>
-        <div id="results-container">
-          <div className={id ? 'half' : ''}>
-            <List {...props} id={id} base={base} />
-          </div>
-
-          {id ? <div>{component({ id, base })}</div> : null}
-        </div>
-      </IonContent>
-
-      <Footer />
-    </>
-  );
-};
-
-const MobileDetail: React.SFC<RouteComponentProps & Props> = ({
+const Detail: React.SFC<RouteComponentProps & Props> = ({
   component,
   id,
   base,
   listings,
 }) => {
   if (!id) {
-    return null;
+    return <Redirect to="/listings" />;
   }
   const listing = listings.find(item => item.id === id);
   if (!listing) {
-    return null;
+    return <Redirect to="/listings" />;
   }
   return (
     <>
-      <Nav title={listing.title} />
+      <TopNav title={listing.title}>
+        <IonButtons slot="start">
+          <IonBackButton defaultHref="/listings" />
+        </IonButtons>
+      </TopNav>
 
       <IonContent>{component({ id, base })}</IonContent>
     </>
   );
 };
 
-const MobileList: React.SFC<RouteComponentProps & Props> = ({
-  base,
-  id,
-  listings,
-  onClick,
-  onSearch,
-  searchValue = '',
-}) => {
-  return (
-    <>
-      <Nav>
-        <SearchBar onSearch={onSearch} searchValue={searchValue} />
-      </Nav>
+class List extends React.PureComponent<RouteComponentProps & Props> {
+  onClick = id => () => this.props.onClick(id);
+  render() {
+    return (
+      <>
+        <Nav>
+          <SearchBar
+            onSearch={this.props.onSearch}
+            searchValue={this.props.searchValue || ''}
+          />
+        </Nav>
 
-      <IonContent>
-        <List base={base} id={id} listings={listings} onClick={onClick} />
-      </IonContent>
-      <Footer />
-    </>
-  );
-};
+        <IonContent>
+          <IonList lines="none">
+            {this.props.listings.length ? (
+              <IonItem color="white">
+                <IonLabel>Found nothing...</IonLabel>
+              </IonItem>
+            ) : (
+              this.props.listings.map((listing, i) => {
+                if (!listing) {
+                  return null;
+                }
+                return (
+                  <Listing
+                    key={listing.id}
+                    priceColor={i === 0 ? 'success' : undefined}
+                    onClick={this.onClick(listing.id)}
+                    listing={listing}
+                  />
+                );
+              })
+            )}
+          </IonList>
+        </IonContent>
+        <Footer />
+      </>
+    );
+  }
+}
 
 const routes: RouteProps[] = [
   {
     path: '/:listingId',
     exact: true,
-    component: MobileDetail,
+    component: Detail,
   },
   {
     path: '/',
     exact: true,
-    component: MobileList,
+    component: List,
   },
 ];
 export class Listings extends React.PureComponent<Props> {
   render() {
-    return (
-      <IsDesktop>
-        {({ isDesktop }) => {
-          if (isDesktop) {
-            return <Desktop {...this.props} />;
-          }
-          return <Routes base="/listings" routes={routes} props={this.props} />;
-        }}
-      </IsDesktop>
-    );
+    return <IonRoutes base="/listings" routes={routes} props={this.props} />;
   }
 }
