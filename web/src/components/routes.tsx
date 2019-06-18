9f43f257fed8c99b09633b303ea7af6992bc860d
obedm503 <obedm503@gmail.com>
2019-06-17 22:10:22 -0700

    bring back IonRoutes. ionic fixed routing and now cleans-up "exited" routes

--- web/src/components/routes.tsx
+++ web/src/components/routes.tsx
@@ -1,4 +1,5 @@
-import { isNil, Omit } from 'lodash';
+import { IonRouterOutlet } from '@ionic/react';
+import { isNil } from 'lodash';
 import * as React from 'react';
 import { Route, RouteProps, Switch } from 'react-router';
 import { normalizeUrl } from '../util';
@@ -9,59 +10,47 @@ type Props = {
   props?: { [key: string]: any };
 };
 
-const route = ({ base, props: rest = {} }: Omit<Props, 'routes'>) => (
-  { path, component, children, render, ...route }: RouteProps,
-  i?: number,
-) => {
-  let fullPath: string | undefined = (base
-    ? normalizeUrl([base].concat(path!))
-    : path) as any;
+const mapRoutes = ({ routes, base, props: rest = {} }: Props) =>
+  routes.map(
+    (
+      { path, component, children, render, ...route }: RouteProps,
+      i?: number,
+    ) => {
+      let fullPath: string | undefined = (base
+        ? normalizeUrl([base].concat(path!))
+        : path) as any;
 
-  // IonRouterOutlet freaks out with undefiend path prop, it shouldn't
-  // if (fullPath === '/' || fullPath === '.') {
-  //   fullPath = undefined;
-  // }
+      // IonRouterOutlet freaks out with undefiend path prop, it shouldn't
+      // if (fullPath === '/' || fullPath === '.') {
+      //   fullPath = undefined;
+      // }
 
-  const Component: any = component || render;
-  const realChildren = !isNil(children)
-    ? children
-    : Component
-    ? props => <Component {...props} {...rest} />
-    : null;
+      const Component: any = component || render;
+      const realChildren = !isNil(children)
+        ? children
+        : Component
+        ? props => <Component {...props} {...rest} />
+        : null;
 
-  return (
-    <Route
-      key={fullPath || i}
-      path={fullPath}
-      children={realChildren}
-      {...route}
-    />
+      return (
+        <Route
+          key={fullPath || i}
+          path={fullPath}
+          children={realChildren}
+          {...route}
+        />
+      );
+    },
   );
-};
 
 export class Routes extends React.Component<Props> {
   render() {
-    const { base, props, routes } = this.props;
-    return <Switch>{routes.map(route({ base, props }))}</Switch>;
+    return <Switch>{mapRoutes(this.props)}</Switch>;
   }
 }
 
-// export const IonRoutes = withRouter(
-//   class IonRoutes extends React.Component<RouteComponentProps & Props> {
-//     render() {
-//       const {
-//         routes,
-//         base,
-//         props,
-//         location: { pathname },
-//       } = this.props;
-//       const matched = routes.find(r => !!matchPath(pathname, r));
-//       if (!matched) {
-//         return null;
-//       }
-
-//       const comp = route({ base, props })(matched);
-//       return <IonRouterOutlet>{comp}</IonRouterOutlet>;
-//     }
-//   },
-// );
+export class IonRoutes extends React.Component<Props> {
+  render() {
+    return <IonRouterOutlet>{mapRoutes(this.props)}</IonRouterOutlet>;
+  }
+}
