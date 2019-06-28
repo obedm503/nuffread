f75480713dcc94d400483d6698cfa1010d41c966
obedm503 <obedm503@gmail.com>
2019-06-28 00:21:13 -0700

    fix and cleanup routes

--- web/src/components/routes.tsx
+++ web/src/components/routes.tsx
@@ -1,5 +1,4 @@
 import { IonRouterOutlet } from '@ionic/react';
-import { isNil } from 'lodash';
 import * as React from 'react';
 import { Route, RouteProps, Switch } from 'react-router';
 import { normalizeUrl } from '../util';
@@ -7,41 +6,22 @@ import { normalizeUrl } from '../util';
 type Props = {
   routes: RouteProps[];
   base?: string;
-  props?: { [key: string]: any };
 };
 
-const mapRoutes = ({ routes, base, props: rest = {} }: Props) =>
-  routes.map(
-    (
-      { path, component, children, render, ...route }: RouteProps,
-      i?: number,
-    ) => {
-      let fullPath: string | undefined = (base
-        ? normalizeUrl([base].concat(path!))
-        : path) as any;
+const mapRoutes = ({ routes, base }: Props) => {
+  return routes.map(({ path, ...route }: RouteProps, i?: number) => {
+    let fullPath: string | undefined = (base
+      ? normalizeUrl([base].concat(path!))
+      : path) as any;
 
-      // IonRouterOutlet freaks out with undefiend path prop, it shouldn't
-      // if (fullPath === '/' || fullPath === '.') {
-      //   fullPath = undefined;
-      // }
+    // IonRouterOutlet freaks out with undefiend path prop, it shouldn't
+    // if (fullPath === '/' || fullPath === '.') {
+    //   fullPath = undefined;
+    // }
 
-      const Component: any = component || render;
-      const realChildren = !isNil(children)
-        ? children
-        : Component
-        ? props => <Component {...props} {...rest} />
-        : null;
-
-      return (
-        <Route
-          key={fullPath || i}
-          path={fullPath}
-          children={realChildren}
-          {...route}
-        />
-      );
-    },
-  );
+    return <Route key={fullPath || i} path={fullPath} {...route} />;
+  });
+};
 
 export class Routes extends React.Component<Props> {
   render() {
