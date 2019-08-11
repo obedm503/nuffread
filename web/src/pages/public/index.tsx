cbc90304a3e182bd844993bbab205b2256b58501
obedm503 <obedm503@gmail.com>
2019-08-10 20:43:03 -0500

    move homepage from /listings to /

--- web/src/pages/public/index.tsx
+++ web/src/pages/public/index.tsx
@@ -1,6 +1,6 @@
 import { IonContent } from '@ionic/react';
 import * as React from 'react';
-import { Redirect, RouteComponentProps } from 'react-router';
+import { RouteComponentProps } from 'react-router';
 import { Footer } from '../../components';
 import { SearchPage } from '../../components/search';
 import { Nav } from './components/nav';
@@ -8,16 +8,13 @@ import { Nav } from './components/nav';
 export default class Public extends React.Component<RouteComponentProps> {
   render() {
     const path = this.props.location.pathname;
-    if (path === '/') {
-      return <Redirect to="/listings" />;
-    }
 
     return (
       <>
-        {path === '/listings' ? <Nav base="/listings" /> : null}
+        {path === '/' ? <Nav base="/" /> : null}
 
         <IonContent>
-          <SearchPage {...this.props} base="/listings" />
+          <SearchPage {...this.props} base="/" />
         </IonContent>
 
         <Footer />
