63c19bde5d8ba63401a591a18fd69899a47c9e23
obedm503 <obedm503@gmail.com>
2019-08-18 21:21:03 -0500

    cleanup my-listings

--- web/src/pages/private/listings.tsx
+++ web/src/pages/private/listings.tsx
@@ -10,10 +10,9 @@ import {
 import gql from 'graphql-tag';
 import { add } from 'ionicons/icons';
 import * as React from 'react';
-import { Query } from 'react-apollo';
-import { Error, IonItemLink, TopNav } from '../../components';
+import { useQuery } from 'react-apollo';
+import { Error, IonButtonLink, TopNav } from '../../components';
 import { BasicListing, BasicListingLoading } from '../../components/listing';
-import { Popover } from '../../components/popover';
 import { BASIC_LISTING } from '../../queries';
 import { IQuery } from '../../schema.gql';
 
@@ -32,16 +31,35 @@ const MY_LISTINGS = gql`
   }
 `;
 
+const Listings: React.FC = () => {
+  const { loading, error, data } = useQuery<IQuery>(MY_LISTINGS);
+  if (loading) {
+    return <BasicListingLoading />;
+  }
+  if (error || !data || !data.me || data.me.__typename !== 'User') {
+    return <Error value={error} />;
+  }
+
+  if (!data.me.listings.length) {
+    return <span>No listings posted</span>;
+  }
+
+  return (
+    <>
+      {data.me.listings.map(listing => {
+        return <BasicListing listing={listing} key={listing.id} />;
+      })}
+    </>
+  );
+};
+
 export const MyListings: React.FC = () => (
   <IonPage>
     <TopNav>
       <IonButtons slot="end">
-        <Popover>
-          <IonItemLink href="/new">
-            <IonIcon slot="start" icon={add} />
-            New Listing
-          </IonItemLink>
-        </Popover>
+        <IonButtonLink href="/new">
+          <IonIcon slot="icon-only" icon={add} />
+        </IonButtonLink>
       </IonButtons>
     </TopNav>
 
@@ -49,35 +67,7 @@ export const MyListings: React.FC = () => (
       <IonGrid>
         <IonRow>
           <IonCol size="12" sizeLg="10" offsetLg="1">
-            <Query<IQuery> query={MY_LISTINGS}>
-              {({ loading, error, data }) => {
-                if (loading) {
-                  return <BasicListingLoading />;
-                }
-                if (
-                  error ||
-                  !data ||
-                  !data.me ||
-                  data.me.__typename !== 'User'
-                ) {
-                  return <Error value={error} />;
-                }
-
-                if (!data.me.listings.length) {
-                  return <span>No listings posted</span>;
-                }
-
-                return (
-                  <>
-                    {data.me.listings.map(listing => {
-                      return (
-                        <BasicListing listing={listing} key={listing.id} />
-                      );
-                    })}
-                  </>
-                );
-              }}
-            </Query>
+            <Listings />
           </IonCol>
         </IonRow>
       </IonGrid>
