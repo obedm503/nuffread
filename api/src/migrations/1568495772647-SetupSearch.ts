8ace3f231670efc78495451b5bd13966bf232897
obedm503 <obedm503@gmail.com>
2019-09-14 19:57:39 -0500

    cleanup

--- api/src/migrations/1568495772647-SetupSearch.ts
+++ api/src/migrations/1568495772647-SetupSearch.ts
@@ -9,22 +9,22 @@ export class SetupSearch1568495772647 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "book" ADD "search_text" text NOT NULL DEFAULT ''`,
     );
-    await queryRunner.query(`
-      UPDATE book SET search_text =
+    await queryRunner.query(
+      `UPDATE book SET search_text =
         unaccent(coalesce(isbn, '')) || ' ' ||
         unaccent(coalesce(title, '')) || ' ' ||
         unaccent(coalesce(sub_title, '')) || ' ' ||
-        unaccent(coalesce(authors, ''))
-    `);
+        unaccent(coalesce(authors, ''))`,
+    );
 
-    await queryRunner.query(`
-      CREATE INDEX book_search_text_idx
+    await queryRunner.query(
+      `CREATE INDEX book_search_text_idx
         ON book
-        USING GIN(search_text gin_trgm_ops)
-    `);
+        USING GIN(search_text gin_trgm_ops)`,
+    );
 
-    await queryRunner.query(`
-      CREATE OR REPLACE FUNCTION book_search_text_function() RETURNS trigger AS $$
+    await queryRunner.query(
+      `CREATE OR REPLACE FUNCTION book_search_text_function() RETURNS trigger AS $$
         begin
           new.search_text :=
             unaccent(coalesce(new.isbn, '')) || ' ' ||
@@ -33,12 +33,12 @@ export class SetupSearch1568495772647 implements MigrationInterface {
             unaccent(coalesce(new.authors, ''));
           return new;
         end
-      $$ LANGUAGE plpgsql
-    `);
-    await queryRunner.query(`
-      CREATE TRIGGER book_search_text_update BEFORE INSERT OR UPDATE
-        ON book FOR EACH ROW EXECUTE PROCEDURE book_search_text_function()
-    `);
+      $$ LANGUAGE plpgsql`,
+    );
+    await queryRunner.query(
+      `CREATE TRIGGER book_search_text_update BEFORE INSERT OR UPDATE
+        ON book FOR EACH ROW EXECUTE PROCEDURE book_search_text_function()`,
+    );
 
     // listings
     await queryRunner.query(
@@ -51,17 +51,18 @@ export class SetupSearch1568495772647 implements MigrationInterface {
       `CREATE INDEX listing_search_text_idx ON listing USING GIN(search_text gin_trgm_ops)`,
     );
 
-    await queryRunner.query(`
-      CREATE OR REPLACE FUNCTION listing_search_text_function() RETURNS trigger AS $$
+    await queryRunner.query(
+      `CREATE OR REPLACE FUNCTION listing_search_text_function() RETURNS trigger AS $$
         begin
           new.search_text := unaccent(coalesce(new.description, ''));
           return new;
         end
-      $$ LANGUAGE plpgsql
-    `);
-    await queryRunner.query(`CREATE TRIGGER listing_search_text_update BEFORE INSERT OR UPDATE
-        ON listing FOR EACH ROW EXECUTE PROCEDURE listing_search_text_function();
-    `);
+      $$ LANGUAGE plpgsql`,
+    );
+    await queryRunner.query(
+      `CREATE TRIGGER listing_search_text_update BEFORE INSERT OR UPDATE
+        ON listing FOR EACH ROW EXECUTE PROCEDURE listing_search_text_function()`,
+    );
   }
 
   async down(queryRunner: QueryRunner) {
