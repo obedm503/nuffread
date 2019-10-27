dd1df518690e88da9dccc0729eec3e20571189a1
obedm503 <obedm503@gmail.com>
2019-10-27 17:09:05 -0500

    private pages: redirect to /home when not matching a valid route

--- web/src/pages/private/index.tsx
+++ web/src/pages/private/index.tsx
@@ -7,37 +7,44 @@ import {
 } from '@ionic/react';
 import { book, home, person } from 'ionicons/icons';
 import * as React from 'react';
-import { Redirect, Route } from 'react-router';
+import { Route, Redirect } from 'react-router';
+import { useRouter } from '../../state/router';
 import { Detail, Home } from './home';
 import { MyListings } from './listings';
 import { Profile } from './profile';
 
-export default class Private extends React.PureComponent {
-  render() {
-    return (
-      <IonTabs>
-        <IonRouterOutlet>
-          <Redirect from="/" exact to="/home" />
-          <Route path="/:tab(listings)" exact component={MyListings} />
-          <Route path="/:tab(profile)" exact component={Profile} />
-          <Route path="/:tab(home)" exact component={Home} />
-          <Route path="/:tab(home)/:listingId" component={Detail} />
-        </IonRouterOutlet>
+const validStarts = ['/home', '/listings', '/profile'];
 
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="home" href="/home">
-            <IonIcon icon={home} ariaLabel="Home" />
-          </IonTabButton>
+export default () => {
+  const { location } = useRouter();
+  const route = location.pathname;
 
-          <IonTabButton tab="listings" href="/listings">
-            <IonIcon icon={book} ariaLabel="Listings" />
-          </IonTabButton>
-
-          <IonTabButton tab="profile" href="/profile">
-            <IonIcon icon={person} ariaLabel="Profile" />
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    );
+  if (!validStarts.some(start => route.startsWith(start))) {
+    return <Redirect to="/home"></Redirect>;
   }
-}
+
+  return (
+    <IonTabs>
+      <IonRouterOutlet>
+        <Route path="/:tab(listings)" exact component={MyListings} />
+        <Route path="/:tab(profile)" exact component={Profile} />
+        <Route path="/:tab(home)" exact component={Home} />
+        <Route path="/:tab(home)/:listingId" component={Detail} />
+      </IonRouterOutlet>
+
+      <IonTabBar slot="bottom">
+        <IonTabButton tab="home" href="/home">
+          <IonIcon icon={home} ariaLabel="Home" />
+        </IonTabButton>
+
+        <IonTabButton tab="listings" href="/listings">
+          <IonIcon icon={book} ariaLabel="Listings" />
+        </IonTabButton>
+
+        <IonTabButton tab="profile" href="/profile">
+          <IonIcon icon={person} ariaLabel="Profile" />
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
+  );
+};
