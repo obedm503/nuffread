632218671b98355fd0dbae6f2443815867013c78
obedm503 <obedm503@gmail.com>
2019-10-26 23:23:06 -0500

    fix search temporarily until ionic 4.11.3

--- web/src/state/search.tsx
+++ web/src/state/search.tsx
@@ -1,4 +1,5 @@
 import { join } from 'path';
+import { useCallback, useState } from 'react';
 import { useRouter } from './router';
 
 export const useSearch = () => {
@@ -6,28 +7,46 @@ export const useSearch = () => {
 
   const searchParams = new URLSearchParams(history.location.search);
 
-  const onSearch = (searchValue: string | undefined) => {
-    let search: string | undefined;
-    if (searchValue) {
-      const query = new URLSearchParams(location.search);
-      query.set('q', searchValue);
-      search = query.toString();
-    }
-    history.push({
-      pathname: location.pathname,
-      search,
-    });
-  };
+  // const onSearch = useCallback(
+  //   (searchValue: string | undefined) => {
+  //     let search: string | undefined;
+  //     if (searchValue) {
+  //       const query = new URLSearchParams(location.search);
+  //       query.set('q', searchValue);
+  //       search = query.toString();
+  //     }
+  //     try {
+  //       history.push({
+  //         pathname: location.pathname,
+  //         search,
+  //       });
+  //     } catch (e) {
+  //       console.warn('history error', e);
+  //     }
+  //   },
+  //   [location, history],
+  // );
+  const [searchValue, setSearch] = useState('');
+  const onSearch = useCallback(
+    (value: string | undefined) => {
+      setSearch(value || '');
+    },
+    [setSearch],
+  );
 
-  const onClick = (id: string) => {
-    history.push({
-      pathname: join(match.url, id),
-      search: searchParams.toString(),
-    });
-  };
+  const onClick = useCallback(
+    (id: string) => {
+      history.push({
+        pathname: join(match.url, id),
+        search: searchParams.toString(),
+      });
+    },
+    [history, match, searchParams],
+  );
 
   return {
-    searchValue: searchParams.get('q') || undefined,
+    // searchValue: searchParams.get('q') || undefined,
+    searchValue,
     onSearch,
     onClick,
   };
