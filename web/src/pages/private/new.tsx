b444e3dc8a3577f80c0d5f3fb0d78c11a1aa33f9
obedm503 <obedm503@gmail.com>
2019-10-29 14:27:49 -0500

    better create listing flow

--- web/src/pages/private/new.tsx
+++ web/src/pages/private/new.tsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from '@apollo/react-hooks';
+import { useApolloClient, useMutation, useQuery } from '@apollo/react-hooks';
 import {
   IonButton,
   IonButtons,
@@ -7,6 +7,7 @@ import {
   IonCardHeader,
   IonCardTitle,
   IonContent,
+  IonFooter,
   IonIcon,
   IonInput,
   IonItem,
@@ -24,7 +25,7 @@ import { ListingBasic } from '../../components/listing-basic';
 import { ListingCard } from '../../components/listing-card';
 import { SafeImg } from '../../components/safe-img';
 import { SearchBar } from '../../components/search-bar';
-import { CREATE_LISTING, SEARCH_GOOGLE } from '../../queries';
+import { CREATE_LISTING, MY_LISTINGS, SEARCH_GOOGLE } from '../../queries';
 import {
   IGoogleBook,
   IListingInput,
@@ -180,143 +181,180 @@ const GOOGLE_BOOK = gql`
 const onCompleted = ({ createListing }: IMutation) => {
   tracker.event('CREATE_LISTING', { price: createListing.price });
 };
-const ConfirmListing: React.FC<{
+const useCreateListing = (variables, closeModal) => {
+  const client = useApolloClient();
+  const [create, { loading, data, error }] = useMutation<
+    IMutation,
+    IListingInput
+  >(CREATE_LISTING, {
+    variables,
+    onCompleted,
+    update: (proxy, { data }) => {
+      const newListing = data && data.createListing;
+      if (!newListing) {
+        return;
+      }
+      const listingsData = client.readQuery<IQuery>({ query: MY_LISTINGS });
+      if (
+        !listingsData ||
+        !listingsData.me ||
+        listingsData.me.__typename !== 'User' ||
+        !listingsData.me.listings
+      ) {
+        return;
+      }
+      const listings = listingsData.me.listings;
+      client.writeQuery({
+        query: MY_LISTINGS,
+        data: {
+          ...listingsData,
+          me: {
+            ...listingsData.me,
+            listings: [newListing, ...listings],
+          },
+        },
+      });
+    },
+  });
+  return {
+    create: React.useCallback(async () => {
+      await create();
+      closeModal();
+    }, [create, closeModal]),
+    loading,
+    error,
+    listing: data && data.createListing,
+  };
+};
+
+const PreviewListing: React.FC<{
   googleId: string;
   price: number;
   description: string;
 }> = ({ googleId, price, description }) => {
-  const [
-    create,
-    { loading: mutLoading, data: mutData, error: mutError },
-  ] = useMutation<IMutation, IListingInput>(CREATE_LISTING, {
-    variables: {
-      googleId,
-      price,
-      description,
-    },
-    onCompleted,
-  });
-
   const { loading: qLoading, data: qData, error: qError } = useQuery<IQuery>(
     GOOGLE_BOOK,
     { variables: { googleId } },
   );
 
-  if (qError || mutError) {
-    return <Error value={qError || mutError}></Error>;
+  if (qError) {
+    return <Error value={qError}></Error>;
   }
-  if (qLoading || mutLoading) {
+  if (qLoading) {
     return <Loading></Loading>;
   }
 
   const googleBook = qData && qData.googleBook;
 
-  const listingPreview = Object.assign(
-    { book: googleBook, price, createdAt: new Date() },
-    mutData && mutData.createListing,
-  );
+  const listingPreview: any = {
+    book: googleBook,
+    price,
+    createdAt: new Date(),
+    description,
+  };
 
-  return (
-    <>
-      <ListingCard listing={listingPreview} />
-
-      {mutData ? null : (
-        <IonButton expand="block" onClick={() => create()}>
-          Create
-        </IonButton>
-      )}
-    </>
-  );
+  return <ListingCard listing={listingPreview} />;
 };
 
-export class CreateListing extends React.PureComponent<
-  { onCancel },
-  {
+const useListingState = () => {
+  const [state, set] = React.useState<{
     searchValue: string;
     googleId: string;
     price?: number;
     description: string;
-  }
-> {
-  state = {
+  }>({
     searchValue: '',
     googleId: '',
     description: '',
     price: undefined,
+  });
+
+  return {
+    state,
+    onPickBook: React.useCallback(
+      book => set({ ...state, googleId: book.googleId }),
+      [state, set],
+    ),
+    onPriceChange: React.useCallback(
+      ({ detail }) => {
+        if (detail.value) {
+          set({ ...state, price: parseFloat(detail.value) * 100 });
+        }
+      },
+      [state, set],
+    ),
+    onDescriptionChange: React.useCallback(
+      ({ detail }) => {
+        if (detail.value) {
+          set({ ...state, description: detail.value });
+        }
+      },
+      [state, set],
+    ),
   };
+};
 
-  onPickBook = book => this.setState({ googleId: book.googleId });
+export const CreateListing = React.memo<{ onCancel }>(({ onCancel }) => {
+  const {
+    state,
+    onPickBook,
+    onDescriptionChange,
+    onPriceChange,
+  } = useListingState();
+  const { create, loading, error, listing } = useCreateListing(state, onCancel);
 
-  onPriceChange = ({ detail }) => {
-    if (detail.value) {
-      this.setState({
-        price: parseFloat(detail.value) * 100,
-      });
-    }
-  };
-  onDescriptionChange = ({ detail }) => {
-    if (detail.value) {
-      this.setState({ description: detail.value });
-    }
-  };
-  render() {
-    const price =
-      typeof this.state.price === 'number'
-        ? (this.state.price! / 100).toFixed(2)
-        : '';
-    return (
-      <IonModal isOpen>
-        <TopNav>
-          <IonButtons slot="secondary">
-            <IonButton onClick={this.props.onCancel}>
-              <IonIcon slot="icon-only" icon={close}></IonIcon>
-            </IonButton>
-          </IonButtons>
-        </TopNav>
+  const price =
+    typeof state.price === 'number' ? (state.price! / 100).toFixed(2) : '';
+  return (
+    <IonModal isOpen>
+      <TopNav>
+        <IonButtons slot="secondary">
+          <IonButton onClick={onCancel}>
+            <IonIcon slot="icon-only" icon={close}></IonIcon>
+          </IonButton>
+        </IonButtons>
+      </TopNav>
 
-        <IonContent>
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>Pick a book</IonCardTitle>
-            </IonCardHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Pick a book</IonCardTitle>
+          </IonCardHeader>
 
-            <IonCardContent>
-              <PickBook
-                onClick={this.onPickBook}
-                activeId={this.state.googleId}
-              />
-            </IonCardContent>
-          </IonCard>
+          <IonCardContent>
+            <PickBook onClick={onPickBook} activeId={state.googleId} />
+          </IonCardContent>
+        </IonCard>
 
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>Details</IonCardTitle>
-            </IonCardHeader>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Details</IonCardTitle>
+          </IonCardHeader>
 
-            <IonCardContent>
-              <IonItem>
-                <IonLabel>Price</IonLabel>
-                <IonIcon icon={logoUsd} size="small"></IonIcon>
-                <IonInput
-                  type="number"
-                  value={price}
-                  debounce={500}
-                  onIonChange={this.onPriceChange}
-                ></IonInput>
-              </IonItem>
+          <IonCardContent>
+            <IonItem>
+              <IonLabel>Price</IonLabel>
+              <IonIcon icon={logoUsd} size="small"></IonIcon>
+              <IonInput
+                type="number"
+                value={price}
+                debounce={500}
+                onIonChange={onPriceChange}
+              ></IonInput>
+            </IonItem>
 
-              <IonItem>
-                <IonLabel position="floating">Description</IonLabel>
-                <IonTextarea
-                  value={this.state.description}
-                  debounce={500}
-                  onIonChange={this.onDescriptionChange}
-                ></IonTextarea>
-              </IonItem>
-            </IonCardContent>
-          </IonCard>
+            <IonItem>
+              <IonLabel position="floating">Description</IonLabel>
+              <IonTextarea
+                value={state.description}
+                debounce={500}
+                onIonChange={onDescriptionChange}
+              ></IonTextarea>
+            </IonItem>
+          </IonCardContent>
+        </IonCard>
 
-          {/* <IonCard>
+        {/* <IonCard>
             <IonCardHeader>
               <IonCardTitle>Upload pictures</IonCardTitle>
             </IonCardHeader>
@@ -324,14 +362,24 @@ export class CreateListing extends React.PureComponent<
             <IonCardContent>upload pictures</IonCardContent>
           </IonCard> */}
 
-          {this.state.googleId ? (
-            <ConfirmListing
-              {...this.state}
-              price={this.state.price || 0}
-            ></ConfirmListing>
-          ) : null}
-        </IonContent>
-      </IonModal>
-    );
-  }
-}
+        {state.googleId ? (
+          <PreviewListing {...state} price={state.price || 0} />
+        ) : null}
+
+        {error ? <Error value={error} /> : null}
+      </IonContent>
+
+      <IonFooter>
+        {!listing ? (
+          <IonButton
+            expand="block"
+            onClick={create}
+            disabled={!state.googleId || !state.price || loading}
+          >
+            Create
+          </IonButton>
+        ) : null}
+      </IonFooter>
+    </IonModal>
+  );
+});
