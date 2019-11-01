2cae3b38cfce25814db1536785b234792e45bfff
obedm503 <obedm503@gmail.com>
2019-11-01 16:29:22 -0500

    cleanup DateResolver

--- api/src/resolvers/date.resolver.ts
+++ api/src/resolvers/date.resolver.ts
@@ -10,16 +10,16 @@ class Config implements GraphQLScalarTypeConfig<Date, string> {
   name = 'Date';
   description = 'Maps between ISO-8601 formatted dates and Date objects';
 
-  parseValue(value: string): Maybe<Date> {
+  parseValue(value: string): Date {
     return new Date(value); // value from the client
   }
 
-  serialize(value: Date): Maybe<string> {
+  serialize(value: Date): string {
     return value.toISOString(); // value sent to the client
   }
   parseLiteral(valueNode: ValueNode): Maybe<Date> {
     if (valueNode.kind === Kind.STRING) {
-      return new Date(valueNode.value); // ast value is always in string format
+      return this.parseValue(valueNode.value); // ast value is always in string format
     }
     return null;
   }
