b7bc808c15230c77b45426a410f239cac139f40e
obedm503 <obedm503@gmail.com>
2019-11-04 15:00:24 -0600

    custom typed `useLazyQuery`

--- web/src/state/apollo.ts
+++ web/src/state/apollo.ts
@@ -1,6 +1,8 @@
 import {
+  LazyQueryHookOptions,
   MutationHookOptions,
   QueryHookOptions,
+  useLazyQuery as apolloLazyQuery,
   useMutation as apolloMutation,
   useQuery as apolloQuery,
 } from '@apollo/react-hooks';
@@ -11,6 +13,11 @@ export const useQuery = <TVariables = never>(
   query: DocumentNode,
   options?: QueryHookOptions<IQuery, TVariables>,
 ) => apolloQuery<IQuery, TVariables>(query, options);
+
+export const useLazyQuery = <TVariables = never>(
+  query: DocumentNode,
+  options?: LazyQueryHookOptions<IQuery, TVariables>,
+) => apolloLazyQuery<IQuery, TVariables>(query, options);
 
 export const useMutation = <TVariables = never>(
   mutation: DocumentNode,
