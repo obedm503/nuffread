39fe58c6794297ecc900fc26208562a5b1006c67
obedm503 <obedm503@gmail.com>
2019-11-22 14:52:47 -0600

    add eye button that toggles the password field visibility

--- web/src/components/controls/password.tsx
+++ web/src/components/controls/password.tsx
@@ -1,9 +1,28 @@
+import { IonButton, IonIcon } from '@ionic/react';
+import { eye, eyeOff } from 'ionicons/icons';
 import * as React from 'react';
 import { ControlProps } from './control';
 import { Text } from './text';
 
-export const Password: React.FC<
-  ControlProps & { autoComplete?: 'current' | 'new' }
-> = ({ autoComplete = 'current', ...props }) => (
-  <Text type="password" {...props} autocomplete="on" required />
-);
+const buttonStyle = { marginTop: 'auto' };
+
+export const Password: React.FC<ControlProps & {
+  autoComplete?: 'current' | 'new';
+}> = React.memo(function Password({ autoComplete = 'current', ...props }) {
+  const [isVisible, setVisible] = React.useState(false);
+  const toggle = React.useCallback(
+    () => setVisible(visibility => !visibility),
+    [setVisible],
+  );
+
+  const type = isVisible ? 'text' : 'password';
+  const icon = isVisible ? eye : eyeOff;
+
+  return (
+    <Text {...props} type={type} autocomplete="on" required>
+      <IonButton slot="end" style={buttonStyle} fill="clear" onClick={toggle}>
+        <IonIcon slot="icon-only" color="medium" icon={icon}></IonIcon>
+      </IonButton>
+    </Text>
+  );
+});
