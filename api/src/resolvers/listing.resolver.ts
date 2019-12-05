7920ba752b3999a91e0f9c94943461bbcad7d296
obedm503 <obedm503@gmail.com>
2019-12-04 19:24:44 -0600

    listing.user is optional if unauthorized simply return undefined instead of throwing an error

--- api/src/resolvers/listing.resolver.ts
+++ api/src/resolvers/listing.resolver.ts
@@ -1,11 +1,13 @@
 import { Listing } from '../entities';
 import { IListing } from '../schema.gql';
-import { ensureUser } from '../util/auth';
+import { userSession } from '../util/auth';
 import { IResolver } from '../util/types';
 
 export const ListingResolver: IResolver<IListing, Listing> = {
   async user({ userId, user }, args, { userLoader, session }) {
-    ensureUser(session);
+    if (!userSession(session)) {
+      return;
+    }
     return user || (await userLoader.load(userId));
   },
   async book({ bookId, book }, args, { bookLoader }) {
