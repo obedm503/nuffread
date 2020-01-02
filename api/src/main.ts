ca209b2d447c952d3ab685d81f48300f29c93744
obedm503 <obedm503@gmail.com>
2020-01-01 20:01:49 -0800

    api: add disabled code for nodejs error handling

--- api/src/main.ts
+++ api/src/main.ts
@@ -114,4 +114,31 @@ apollo.applyMiddleware({
     await close();
     process.kill(process.pid, 'SIGUSR2');
   });
+
+  // based on https://blog.heroku.com/best-practices-nodejs-errors
+  // function terminate(server: Server) {
+  //   return (code: number, reason: string) => err => {
+  //     // Exit function
+  //     const exit = () => {
+  //       logger.info(reason);
+  //       process.env.CORE_DUMP ? process.abort() : process.exit(code);
+  //     };
+
+  //     if (err && err instanceof Error) {
+  //       // Log error information, use a proper logging library here :)
+  //       logger.fatal(err.message, err.stack);
+  //     }
+
+  //     // Attempt a graceful shutdown
+  //     server.close(exit);
+  //     setTimeout(exit, 1500).unref();
+  //   };
+  // }
+
+  // const exitHandler = terminate(server);
+
+  // process.on('uncaughtException', exitHandler(1, 'Unexpected Error'));
+  // process.on('unhandledRejection', exitHandler(1, 'Unhandled Promise'));
+  // process.on('SIGTERM', exitHandler(0, 'SIGTERM'));
+  // process.on('SIGINT', exitHandler(0, 'SIGINT'));
 })().catch(e => logger.fatal(e));
