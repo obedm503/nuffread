51b5ec3e3a46d9eb3eb2f3e478390d148ecee573
obedm503 <obedm503@gmail.com>
2020-03-19 15:49:01 -0500

    api: enfore https in thumbnails

--- api/src/resolvers/book.ts
+++ api/src/resolvers/book.ts
@@ -1,7 +1,14 @@
 import { IBookResolvers } from '../schema.gql';
 import { paginationOptions, sameSchoolListings } from '../util';
+import { processImageLink } from '../util/google-books';
 
 export const BookResolver: IBookResolvers = {
+  thumbnail({ thumbnail }) {
+    if (!thumbnail) {
+      return;
+    }
+    return processImageLink(thumbnail);
+  },
   async listings({ id }, { paginate }, { session, getMe }) {
     const { take, skip } = paginationOptions(paginate);
 
