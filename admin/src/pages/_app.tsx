4da50b86a55667f0449490bea36f3517e2173a6d
obedm503 <obedm503@gmail.com>
2021-03-15 00:12:31 -0500

    finish update

--- admin/src/pages/_app.tsx
+++ admin/src/pages/_app.tsx
@@ -1,24 +1,18 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles.css';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { useApollo } from '../apollo';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const apolloClient = useApollo(pageProps.initialApolloState);
-
   return (
-    <ApolloProvider client={apolloClient}>
-      <>
-        <Head>
-          <title>Admin | Nuffread</title>
-          <link rel="icon" href="https://www.nuffread.com/favicon.ico" />
-        </Head>
+    <>
+      <Head>
+        <title>Admin | Nuffread</title>
+        <link rel="icon" href="https://www.nuffread.com/favicon.ico" />
+      </Head>
 
-        <div className="absolute min-h-full w-full min-w-full bg-tertiary">
-          <Component {...pageProps} />
-        </div>
-      </>
-    </ApolloProvider>
+      <div className="absolute min-h-full w-full min-w-full bg-tertiary">
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
