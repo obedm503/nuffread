b0cdee3bcd2566ad91b4a17eb58ab2a48c3b9404
obedm503 <obedm503@gmail.com>
2022-03-19 18:38:14 -0500

    fix gql uri

--- beta/src/apollo/with-apollo.tsx
+++ beta/src/apollo/with-apollo.tsx
@@ -34,7 +34,7 @@ function createApolloClient(req: Req, res: Res) {
   }
 
   const httpLink = new HttpLink({
-    uri: `${process.env.NEXT_PUBLIC_SELF_ORIGIN}/graphql`,
+    uri: ssrMode ? process.env.NEXT_PUBLIC_GRAPHQL_API : '/graphql',
     credentials: 'include',
     headers,
   });
